ec9a7bb86ab6b1f646667d7f1a501361d01df79c
Eugenia <eugenia.mariotti@live.com.ar>
2015-05-31 02:50:11 -0300

    correccion Celda + CeldaTests

---
+++ CeldaTests.java
@@ -0,0 +1,43 @@
+package algoritmos_3.starcraft.mapa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CeldaTests {
+
+	@Test
+	public void nuevaCeldaCreaCeldaConEstadoNoOcupado(){
+		
+		Celda celda=new Celda(1,1);
+		assert(celda.estaOcupada());
+		
+	}
+	
+	@Test
+	public void celdaGetAbcisaDevuelveCoordenadaHorizontalDeLaCelda(){
+		
+		Celda celda=new Celda(1,2);
+		assertEquals(celda.getAbcisa(),1);
+		
+	}
+	
+	@Test
+	public void celdaGetOrdenadaDevuelveCoordenadaVerticalDeLaCelda(){
+		
+		Celda celda=new Celda(1,2);
+		assertEquals(celda.getOrdenada(),2);
+		
+	}
+	
+	@Test
+	public void celdaEstaOcupadaDeberiaDevolverFalseLuegoDeOcuparCelda(){
+		
+		Celda celda=new Celda(1,4);
+		celda.Ocupar();
+		assertFalse(celda.estaOcupada());
+		
+	}
+
+
+}
