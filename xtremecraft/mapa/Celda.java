d9925a40112f0f6a10544af807fc55652c774c5e
Eugenia <eugenia.mariotti@live.com.ar>
2015-05-31 03:31:05 -0300

    subo paquete que contiene las clases Celda.java y CeldaTests.java

---
+++ xtremecraft/mapa/Celda.java
@@ -0,0 +1,42 @@
+package xtremecraft.mapa;
+
+public class Celda {
+	
+	private int coordenadaX;
+	private int coordenadaY;
+	private boolean ocupada;
+	
+	public Celda(int X,int Y){
+		
+		this.coordenadaX=X;
+		this.coordenadaY=Y;
+		this.ocupada=false;
+		
+	}
+	
+	public boolean estaOcupada(){
+		
+		return this.ocupada;
+		
+	}
+	
+	public int getAbcisa(){
+		
+		return this.coordenadaX;
+		
+	}
+
+	public int getOrdenada() {
+		
+		return this.coordenadaY;
+		
+	}
+
+	public void Ocupar() {
+		
+		this.ocupada=false;
+		
+	}
+	
+
+}
