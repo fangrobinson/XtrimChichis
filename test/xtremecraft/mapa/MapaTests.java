28c590926602f2f91bc8379a89dcab8e93f6499c
Robinson Fang <robinsonfang@MacBook-Pro-de-Robinson.local>
2015-05-31 22:47:41 -0300

    Agrego Tests para la clase mapa

---
+++ test/xtremecraft/mapa/MapaTests.java
@@ -0,0 +1,25 @@
+package xtremecraft.mapa;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MapaTests {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void CrearMapaConCantJugadoresNegativoDaError(){
+		
+		new Mapa(-1);
+		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void CrearMapaConCantJugadoresCeroDaError(){
+		
+		new Mapa(0);
+		
+	}
+	
+}
