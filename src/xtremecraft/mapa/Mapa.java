50e1950c3aafa0e8e31e65a631d445e53de53af8
Robinson Fang <fang.robinson@gmail.com>
2015-06-01 17:34:29 -0300

    avanzo en la clase mapa

--- src/xtremecraft/mapa/Mapa.java
+++ src/xtremecraft/mapa/Mapa.java
@@ -4,26 +4,45 @@ import java.util.TreeMap;
 
 public class Mapa {
 	
-	private TreeMap<Integer, Celda> mapa;
+	private TreeMap<Integer, TreeMap<Integer, Celda>> mapaAlto;
+	private int alto;
+	private int ancho;
 
 	public Mapa(int cant_jugadores) {
 		
 		if (cant_jugadores <= 1){
 			throw new IllegalArgumentException("La cantidad de jugadores debe ser un numero positivo");
 		}
+		this.alto = this.decidirAlto(cant_jugadores);
+		this.ancho = this.decidirAncho(cant_jugadores);
 		
-		
-		
-		this.mapa = new TreeMap<Integer, Celda>();
-		
+		rellenarMapa(alto, ancho);	
 	}
 
+	private void rellenarMapa(int alto, int ancho) {
+		this.mapaAlto = new TreeMap<Integer, TreeMap<Integer, Celda>>();
+		for(int i =0; i < this.alto ;i = i + 1) {
+			this.mapaAlto.put(i, new TreeMap<Integer, Celda>());
+			for(int j = 0; j < this.ancho; j = j + 1) {
+				this.mapaAlto.get(i).put(j, new Celda(j, i));
+			}
+		}	
+	}
+	
 	public boolean tieneAire() {
 		return true;
 	}
 
 	public boolean tieneTierra() {
 		return true;
+	}
+	
+	private int decidirAlto(int cant_jugadores) {
+		return cant_jugadores * 25;
+	}
+
+	private int decidirAncho(int cant_jugadores) {
+		return cant_jugadores * 100;
 	}
 	
 }
