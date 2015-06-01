baa68105024bf466bf95ae87c29cd981283416cb
Robinson Fang <robinsonfang@MacBook-Pro-de-Robinson.local>
2015-06-01 15:57:04 -0300

    Agrego codigo a la clase mapa

--- src/xtremecraft/mapa/Mapa.java
+++ src/xtremecraft/mapa/Mapa.java
@@ -1,12 +1,18 @@
 package xtremecraft.mapa;
 
+import java.util.TreeMap;
+
 public class Mapa {
 	
+	private TreeMap<Integer, Celda> mapa;
+
 	public Mapa(int cant_jugadores) {
 		
-		if (cant_jugadores <= 0){
+		if (cant_jugadores <= 1){
 			throw new IllegalArgumentException("La cantidad de jugadores debe ser un numero positivo");
 		}
+		
+		this.mapa = new TreeMap<Integer, Celda>();
 		
 	}
 	
