b1e280fa0ddd70bcb4d882f117855ec8187f41b2
Robinson Fang <fang.robinson@gmail.com>
2015-06-09 15:14:14 -0300

    Agrego cambios en la interfaz ubicable

--- src/xtremecraft/interfaces/Ubicable.java
+++ src/xtremecraft/interfaces/Ubicable.java
@@ -5,5 +5,11 @@ import xtremecraft.mapa.Coordenada;
 public interface Ubicable {
 	
 	public Coordenada getUbicacionActual();
+	
+	public void actualizarUbicacion();
+	
+	public boolean puedeUbicarseEnTierra(int columna, int fila);
+	
+	public boolean puedeUbicarseEnAire();
 
 }
