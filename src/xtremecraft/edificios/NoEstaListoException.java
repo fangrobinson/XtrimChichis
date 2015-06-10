1f4387c921049241d65d2cc29a18dde05f6e252d
Robinson Fang <fang.robinson@gmail.com>
2015-06-09 23:37:46 -0300

    Agrego pruebas del manejo de expeciones

---
+++ src/xtremecraft/edificios/NoEstaListoException.java
@@ -0,0 +1,9 @@
+package xtremecraft.edificios;
+
+@SuppressWarnings("serial")
+public class NoEstaListoException extends Exception {
+	
+	public NoEstaListoException(String message) { super(message); }
+
+
+}
