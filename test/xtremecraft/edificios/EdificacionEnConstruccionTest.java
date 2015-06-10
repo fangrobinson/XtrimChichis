1e13a6da63ce5b0767ba934dfe256a59a3b1a819
Robinson Fang <fang.robinson@gmail.com>
2015-06-09 23:13:52 -0300

    tests que abarcan mas casos

--- test/xtremecraft/edificios/EdificacionEnConstruccionTest.java
+++ test/xtremecraft/edificios/EdificacionEnConstruccionTest.java
@@ -1,6 +1,7 @@
 package xtremecraft.edificios;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -22,6 +23,16 @@ public class EdificacionEnConstruccionTest {
 	}
 	
 	@Test
+	public void creoBarracaEnConstruccionNoEstaLista(){
+		Terran nuevoClanTerran = new Terran();
+		Edificio unaBarraca = Barraca.nuevaBarraca(nuevoClanTerran, 1, 1);
+		EdificacionEnConstruccion edif = new EdificacionEnConstruccion(unaBarraca);
+	
+		assertFalse(edif.estaListo());
+		
+	}
+	
+	@Test
 	public void creoBarracaEnConstruccionSuVidaAumentaAlPasarElTiempo(){
 		Terran nuevoClanTerran = new Terran();
 		Edificio unaBarraca = Barraca.nuevaBarraca(nuevoClanTerran, 1, 1);
@@ -33,4 +44,36 @@ public class EdificacionEnConstruccionTest {
 		assertTrue(vida > 0);
 		
 	}
+	
+	@Test
+	public void alPasarElTiempoNecesarioBarracaEstaLista(){
+		Terran nuevoClanTerran = new Terran();
+		Edificio unaBarraca = Barraca.nuevaBarraca(nuevoClanTerran, 1, 1);
+		int tiempo = unaBarraca.tiempoConstruccion();
+		EdificacionEnConstruccion edif = new EdificacionEnConstruccion(unaBarraca);
+		
+		for(int i = 0; i < tiempo ; i = i + 1){
+			edif.pasarTiempo();
+		}
+		
+		assertTrue(edif.estaListo());
+		
+	}
+	
+	@Test
+	public void alPasarTiempoInsuficienteBarracaNoEstaLista(){
+		Terran nuevoClanTerran = new Terran();
+		Edificio unaBarraca = Barraca.nuevaBarraca(nuevoClanTerran, 1, 1);
+		int tiempo = unaBarraca.tiempoConstruccion();
+		EdificacionEnConstruccion edif = new EdificacionEnConstruccion(unaBarraca);
+		
+		for(int i = 0; i < tiempo - 1 ; i = i + 1){
+			edif.pasarTiempo();
+		}
+		
+		assertFalse(edif.estaListo());
+		
+	}
+	
+	
 }
