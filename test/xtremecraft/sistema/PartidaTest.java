4fcbbd34c00ba230a96b3b96fc7785b56e08ce35
Ana Czarnitzki <czarniana@gmail.com>
2015-06-16 22:48:26 -0300

    La clase Juego ahora se llama Partida.

--- test/xtremecraft/sistema/PartidaTest.java
+++ test/xtremecraft/sistema/PartidaTest.java
@@ -8,34 +8,34 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-public class JuegoTest {
+public class PartidaTest {
 	
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void CrearJuegoConCantJugadoresCeroDaError(){
+	public void CrearPartidaConCantJugadoresCeroDaError(){
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
-		new Juego(jugadores);
+		new Partida(jugadores);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void CrearJuegoConCantJugadoresUnoDaError(){
+	public void CrearPartidaConCantJugadoresUnoDaError(){
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 	    Jugador ninioRata = new Jugador("Bart");
 		jugadores.add(ninioRata);
 	    
-		new Juego(jugadores);
+		new Partida(jugadores);
 	}
 	
 	@Test
-	public void creoUnJuegoElTiempoEsCero(){
+	public void creoUnaPartidaElTiempoEsCero(){
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 	    Jugador ninioRata = new Jugador("Bart");
 	    Jugador noob = new Jugador("Jose");
 		jugadores.add(ninioRata);
 		jugadores.add(noob);
 	
-		Juego juego = new Juego(jugadores);
-		int tiempo = juego.tiempo();
+		Partida partida = new Partida(jugadores);
+		int tiempo = partida.tiempo();
 		
 		assertEquals(tiempo, 0);
 	}
@@ -48,27 +48,27 @@ public class JuegoTest {
 		jugadores.add(ninioRata);
 		jugadores.add(noob);
 	
-		Juego juego = new Juego(jugadores);
+		Partida partida = new Partida(jugadores);
 		
-		juego.pasarTiempo();
-		int tiempo = juego.tiempo();
+		partida.pasarTiempo();
+		int tiempo = partida.tiempo();
 		
 		assertTrue(tiempo > 0);
 	}
 	
 	@Test
-	public void creoUnJuegoYPuedoConocerQuienEmpieza (){
+	public void creoUnaPartidaYPuedoConocerQuienEmpieza (){
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 	    Jugador ninioRata = new Jugador("Bart");
 	    Jugador noob = new Jugador("Jose");
 		jugadores.add(ninioRata);
 		jugadores.add(noob);
 	
-		Juego juego = new Juego(jugadores);
+		Partida partida = new Partida(jugadores);
 		
 		Jugador jug = null;
 		
-		jug= juego.quienJuega();
+		jug= partida.quienJuega();
 
 		assertTrue(jug.getClass() == Jugador.class);
 	}
@@ -81,56 +81,56 @@ public class JuegoTest {
 		jugadores.add(ninioRata);
 		jugadores.add(noob);
 	
-		Juego juego = new Juego(jugadores);
+		Partida partida = new Partida(jugadores);
 		
 		Jugador jug = null;
 		Jugador jug2 = null;
 		
-		jug = juego.quienJuega();
-		juego.pasarTiempo();
-		jug2 = juego.quienJuega(); 
+		jug = partida.quienJuega();
+		partida.pasarTiempo();
+		jug2 = partida.quienJuega(); 
 
 		assertFalse(jug == jug2);
 	}
 	
 	@Test
-	public void enUnJuegoDeDosJugadoresSePuedeVerQueLaRondaVuelveAEmpezarDespuesDePasar2Turnos (){
+	public void enUnaPartidaDeDosJugadoresSePuedeVerQueLaRondaVuelveAEmpezarDespuesDePasar2Turnos (){
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 	    Jugador ninioRata = new Jugador("Bart");
 	    Jugador noob = new Jugador("Jose");
 		jugadores.add(ninioRata);
 		jugadores.add(noob);
 	
-		Juego juego = new Juego(jugadores);
+		Partida partida = new Partida(jugadores);
 		
 		Jugador jug = null;
 		Jugador jug2 = null;
 		
-		jug = juego.quienJuega();
-		juego.pasarTiempo();
-		juego.pasarTiempo();
-		jug2 = juego.quienJuega(); 
+		jug = partida.quienJuega();
+		partida.pasarTiempo();
+		partida.pasarTiempo();
+		jug2 = partida.quienJuega(); 
 
 		assertTrue(jug == jug2);
 	}
 	
 	@Test
-	public void enUnJuegoDeDosJugadoresSePuedeVerQueNoSeRepiteElJugador (){
+	public void enUnaPartidaDeDosJugadoresSePuedeVerQueNoSeRepiteElJugador (){
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 	    Jugador ninioRata = new Jugador("Bart");
 	    Jugador noob = new Jugador("Jose");
 		jugadores.add(ninioRata);
 		jugadores.add(noob);
 	
-		Juego juego = new Juego(jugadores);
+		Partida partida = new Partida(jugadores);
 		
 		Jugador jug = null;
 		Jugador jug2 = null;
 		
-		juego.pasarTiempo();
-		jug = juego.quienJuega();
-		juego.pasarTiempo();
-		jug2 = juego.quienJuega(); 
+		partida.pasarTiempo();
+		jug = partida.quienJuega();
+		partida.pasarTiempo();
+		jug2 = partida.quienJuega(); 
 
 		assertFalse(jug == jug2);
 	}
