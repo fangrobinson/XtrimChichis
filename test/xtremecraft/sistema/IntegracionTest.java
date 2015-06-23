6380aa35cee1d8817bfa8ac4a73ae9d08cbe26a9
Ana Czarnitzki <czarniana@gmail.com>
2015-06-23 00:45:57 -0300

    Empiezo las pruebas de integración.

---
+++ test/xtremecraft/sistema/IntegracionTest.java
@@ -0,0 +1,11 @@
+package xtremecraft.sistema;
+
+import org.junit.Test;
+
+public class IntegracionTest {
+	
+	@Test
+	public void prueba01_creacionDeRecolectoresDeGasDeUnidadesAtaqueEntreUnidadesYaEdificios(){
+		
+	}
+}
