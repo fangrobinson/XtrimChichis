6cc77f58213720c5a4ee66f39532df48c9a32206
Ana Czarnitzki <czarniana@gmail.com>
2015-06-23 01:39:02 -0300

    Subo la vs alpha de las pruebas de integración, se rompe, halp.

--- test/xtremecraft/sistema/IntegracionTest.java
+++ test/xtremecraft/sistema/IntegracionTest.java
@@ -1,11 +1,35 @@
 package xtremecraft.sistema;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import xtremecraft.mapa.Terreno;
 
 public class IntegracionTest {
 	
 	@Test
 	public void prueba01_creacionDeRecolectoresDeGasDeUnidadesAtaqueEntreUnidadesYaEdificios(){
+		ArrayList<String> nombresJugadores = new ArrayList<String>();
+		nombresJugadores.add("noob");
+		nombresJugadores.add("playerOpCoreano");
+		
+		Partida xtremGame = new Partida(nombresJugadores);
+		
+		String nombreJugadorTurno = xtremGame.quienJuega().nombre();
+		
+		assertEquals(nombreJugadorTurno, "noob");
+		
+		Jugador jugadorTurno = xtremGame.quienJuega();
+		//xtremGame.crearRecolectorDeMineral(jugadorTurno, 1, 5);
+		
+		xtremGame.pasarTiempo();
+		
+        nombreJugadorTurno = xtremGame.quienJuega().nombre();
+		
+		//assertEquals(nombreJugadorTurno, "playerOpCoreano");
 		
 	}
 }
