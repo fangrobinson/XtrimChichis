7336dbae0274ae45f8f5de54e47261772e4b7616
Ana Czarnitzki <czarniana@gmail.com>
2015-06-23 22:24:39 -0300

    Agrego los colores para las Vistas de Gas, Minerales y Tierra.

--- src/vista/VistaMinerales.java
+++ src/vista/VistaMinerales.java
@@ -1,6 +1,13 @@
 package vista;
 
+import java.awt.Color;
+
 @SuppressWarnings("serial")
 public class VistaMinerales extends Vista {
+	
+	public VistaMinerales(){
+		
+		this.setBackground(new Color(0,204,204));
+	}
 
 }
