f1e2571e8e4f7ff4f0da51230fa05186202e8610
Eugenia <eugenia.mariotti@live.com.ar>
2015-06-24 22:32:40 -0300

    Inicio vista

--- src/vista/StarCraftVista.java
+++ src/vista/StarCraftVista.java
@@ -3,6 +3,13 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import java.awt.Color;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JToggleButton;
 
 public class StarCraftVista {
 
@@ -35,10 +42,27 @@ public class StarCraftVista {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frame = new JFrame("AlgoCraft");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("450px"),},
+			new RowSpec[] {
+				RowSpec.decode("31px"),
+				RowSpec.decode("216px"),
+				RowSpec.decode("25px"),}));
+		
+		JToolBar toolBar = new JToolBar();
+		frame.getContentPane().add(toolBar, "1, 1, fill, top");
+		
+		JButton btnNuevoJuego = new JButton("Nuevo Juego");
+		btnNuevoJuego.setBackground(Color.LIGHT_GRAY);
+		toolBar.add(btnNuevoJuego);
+		
+		JToggleButton tglbtnPasarTurno = new JToggleButton("PASAR TURNO");
+		tglbtnPasarTurno.setBackground(Color.RED);
+		tglbtnPasarTurno.setForeground(Color.WHITE);
+		frame.getContentPane().add(tglbtnPasarTurno, "1, 3");
 	}
 
 }
