6c4e419e7b60242f9a6a5fde256ec61a807f96ac
Robinson Fang <fang.robinson@gmail.com>
2015-06-29 20:38:56 -0300

    agregada barra de menu

--- src/xtremecraft/vista/VentanaDeAlgoCraft.java
+++ src/xtremecraft/vista/VentanaDeAlgoCraft.java
@@ -1,11 +1,16 @@
 package xtremecraft.vista;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 import xtremecraft.edificios.Barraca;
@@ -28,7 +33,64 @@ import xtremecraft.unidades.NaveTransporte;
 public class VentanaDeAlgoCraft  extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
+	
 	private HashMap<Class<?>, Class<?>> vistas;
+
+	public VentanaDeAlgoCraft(Partida partida) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+		
+		this.vistas = this.generarVistas();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Algo Craft Game");
+		setPreferredSize(new Dimension(1200, 950));
+		
+        JMenuBar menuBarra = crearBarraDeMenu();
+        setJMenuBar(menuBarra);
+
+		setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+		
+		JPanel sectorJuego = new SectorJuego(partida, this.vistas);
+		sectorJuego.setPreferredSize(new Dimension(1000, 600));
+		setVisible(true);
+		JPanel panelInformacion = new SectorInformacionParaElUsuario(partida, vistas);
+		panelInformacion.setPreferredSize(new Dimension(1000, 100));
+		
+		sectorJuego.setVisible(true);
+		panelInformacion.setVisible(true);
+		this.add(sectorJuego);
+		this.add(panelInformacion);
+		pack();
+		setResizable(false);
+		setVisible(true);
+		
+	}
+
+	private static JMenuBar crearBarraDeMenu() {
+        JMenuBar barraDeMenu = new JMenuBar();
+
+        JMenu menuArchivo = new JMenu("Archivo");
+        JMenu menuAbout = new JMenu("Sobre Nosotros");
+        barraDeMenu.add(menuArchivo);
+        barraDeMenu.add(menuAbout);
+        
+        JMenuItem nuevoJuego = new JMenuItem("Nuevo Juego");
+        JMenuItem salir = new JMenuItem("Salir");
+        salir.addActionListener(new OpcionSalirListener());
+        menuArchivo.add(nuevoJuego);
+        menuArchivo.add(salir);
+        
+		return barraDeMenu;
+		
+	}
+	
+
+	static class OpcionSalirListener implements ActionListener{
+		
+        public void actionPerformed(ActionEvent evento) {
+        	System.exit(0);
+        }
+		
+	}
 	
 	private HashMap<Class<?>, Class<?>> generarVistas() {
 		
@@ -52,32 +114,4 @@ public class VentanaDeAlgoCraft  extends JFrame{
 		
 		return vistas;
 	}
-
-	public VentanaDeAlgoCraft(Partida partida) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
-		
-		this.vistas = this.generarVistas();
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Algo Craft Game");
-		setPreferredSize(new Dimension(1200, 950));
-		setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
-		
-		//Agregar aca los componentes de este Frame
-		JPanel sectorJuego = new SectorJuego(partida, this.vistas);
-		sectorJuego.setPreferredSize(new Dimension(1000, 600));
-		setVisible(true);
-		JPanel panelInformacion = new SectorInformacionParaElUsuario(partida, vistas);
-		panelInformacion.setPreferredSize(new Dimension(1000, 150));
-		
-		//Ponerlos visibles
-		sectorJuego.setVisible(true);
-		panelInformacion.setVisible(true);
-		this.add(sectorJuego);
-		this.add(panelInformacion);
-		pack();
-		setResizable(false);
-		setVisible(true);
-		
-	}
-
 }
