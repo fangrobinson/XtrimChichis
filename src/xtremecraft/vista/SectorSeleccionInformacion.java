9d96f66a04eca18726f5eb01683435e34a03998b
Eugenia <eugenia.mariotti@live.com.ar>
2015-06-30 16:40:24 -0300

    Borro idea vieja.

--- src/xtremecraft/vista/SectorSeleccionInformacion.java
+++ src/xtremecraft/vista/SectorSeleccionInformacion.java
@@ -1,34 +1,25 @@
 package xtremecraft.vista;
 
 import java.awt.Color;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Observable;
-import java.util.Observer;
 
-import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
-import xtremecraft.mapa.Coordenada;
 import xtremecraft.partida.Partida;
 
-public class SectorSeleccionInformacion extends JPanel implements Observer{
+public class SectorSeleccionInformacion extends JPanel{
 
 	private static final long serialVersionUID = 1L;
-	private JComboBox<String> accionesDisponiblesJugador;
 	private AccionesDisponibles panelAccionesDisponibles;
-	private Partida partida;
-	private MapaObservable mapa;
+	//private Partida partida;
+	//private MapaObservable mapa;
 	
 	public SectorSeleccionInformacion(MapaObservable mapaObservable,Partida partida){
 		
-		this.mapa = mapaObservable;
+		//this.mapa = mapaObservable;
 		setLayout(new BoxLayout(this ,BoxLayout.Y_AXIS));
 		setBackground(new Color(255,255,255));
-		
-		mapaObservable.agregarObservadorAVistas(this);
-		
+				
 		JPanel panelSeleccionObjeto = new SectorEstadoObjetoSeleccionado(mapaObservable,partida.getMapa());
 		
 		this.add(panelSeleccionObjeto);
@@ -41,35 +32,6 @@ public class SectorSeleccionInformacion extends JPanel implements Observer{
 		
 		setVisible(true);
 
-	}
-
-	@Override
-	public void update(Observable observado, Object arg1) {
-		
-		ObservableSeleccionado observadoActual = (ObservableSeleccionado) observado;
-		Coordenada coordenada = observadoActual.getCoordenadaActualSeleccionado();
-		//original:
-		//JComboBox opciones = observadoActual.getAccionesDisponibles();
-		//prueba:
-		String[] opcionesPrueba = {"Construir barraca", "Construir deposito"}; 
-		this.accionesDisponiblesJugador = new JComboBox<String>(opcionesPrueba);
-		Class<?> claseControlador = observadoActual.getClaseControladorDeAccion();
-		AbstractAction controlador;
-		try {
-			////pasarle el mapaObservable al controlador para que lo actualize??
-			controlador = (AbstractAction) claseControlador.getDeclaredConstructor(Partida.class,JComboBox.class,Coordenada.class).newInstance(this.partida,this.accionesDisponiblesJugador,coordenada);
-			this.accionesDisponiblesJugador.addActionListener(controlador);
-			
-		} catch (InstantiationException | IllegalAccessException
-				| IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		this.panelAccionesDisponibles.actualizarPanel(this.accionesDisponiblesJugador);
-	
-		
 	}
 	
 }
