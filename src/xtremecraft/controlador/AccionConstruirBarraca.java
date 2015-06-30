c86ba63622c3777ff5fd87d084c0db99401d095e
Ana Czarnitzki <czarniana@gmail.com>
2015-06-30 18:20:32 -0300

    Agregado el AccionConstruirBarraca.

---
+++ src/xtremecraft/controlador/AccionConstruirBarraca.java
@@ -0,0 +1,34 @@
+package xtremecraft.controlador;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import xtremecraft.mapa.Coordenada;
+import xtremecraft.mapa.NoSePudoOcuparElTerrenoException;
+import xtremecraft.mapa.Tierra;
+import xtremecraft.partida.Jugador;
+import xtremecraft.partida.Partida;
+import xtremecraft.raza.RecursosInsuficientesException;
+import xtremecraft.vista.MensajeDeError;
+
+@SuppressWarnings("serial")
+public class AccionConstruirBarraca extends AbstractAction{
+
+	private Partida partida;
+	private Coordenada coordenada;
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Jugador jugadorActual = partida.quienJuega();
+		Tierra tierraConstruccion = (Tierra) partida.getMapa().getCeldaEnFilaColumna(coordenada.fila(), coordenada.columna()).getCapaInferior();
+		try{
+			jugadorActual.crearBarraca(tierraConstruccion);	
+		}catch(RecursosInsuficientesException noHayRecursos){
+			 new MensajeDeError("No se tienen suficientes recursos");
+		}catch(NoSePudoOcuparElTerrenoException terrenoOcupado){
+			new MensajeDeError("El terreno seleccionado no está disponible para construir");
+		}
+	}
+	
+}
