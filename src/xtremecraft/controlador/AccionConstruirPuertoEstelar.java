c8ee4debe1bffefefec0778f6fd2c894cbc0e365
Ana Czarnitzki <czarniana@gmail.com>
2015-06-30 18:38:47 -0300

    Agrego AccionConstruir de Recolectores y de PuertoEstelar.

---
+++ src/xtremecraft/controlador/AccionConstruirPuertoEstelar.java
@@ -0,0 +1,45 @@
+package xtremecraft.controlador;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import xtremecraft.mapa.Coordenada;
+import xtremecraft.mapa.NoSePudoOcuparElTerrenoException;
+import xtremecraft.mapa.Tierra;
+import xtremecraft.partida.Jugador;
+import xtremecraft.partida.Partida;
+import xtremecraft.raza.RazaNoTieneFabricasException;
+import xtremecraft.raza.RecursosInsuficientesException;
+import xtremecraft.vista.MensajeDeError;
+
+@SuppressWarnings("serial")
+public class AccionConstruirPuertoEstelar extends AbstractAction{
+
+	private Partida partida;
+	private Coordenada coordenada;
+	
+	public AccionConstruirPuertoEstelar(Partida partida, Coordenada coordenada){
+		
+		super("ConstruirPuertoEstelar");
+		this.coordenada = coordenada;
+		this.partida = partida;
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Jugador jugadorActual = partida.quienJuega();
+		Tierra tierraConstruccion = (Tierra) partida.getMapa().getCeldaEnFilaColumna(coordenada.fila(), coordenada.columna()).getCapaInferior();
+		try{
+			jugadorActual.crearPuertoEstelar(tierraConstruccion);	
+		}catch(RecursosInsuficientesException noHayRecursos){
+			 new MensajeDeError("No se tienen suficientes recursos");
+		}catch(NoSePudoOcuparElTerrenoException terrenoOcupado){
+			new MensajeDeError("El terreno seleccionado no está disponible para construir");
+		}catch(RazaNoTieneFabricasException noHayFabricas){
+			new MensajeDeError("Se necesita una fabrica para crear una fabrica");
+		}
+	}
+
+}
