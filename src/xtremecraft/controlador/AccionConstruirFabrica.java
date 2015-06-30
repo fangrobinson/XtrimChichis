a12b56b3123c1c7eac80e70489ddbb38f0d3f8a8
Ana Czarnitzki <czarniana@gmail.com>
2015-06-30 18:27:25 -0300

    Agrego AccionConstruirFabrica, arreglo warnings en AccionConstruirBarraca.

--- src/xtremecraft/controlador/AccionConstruirFabrica.java
+++ src/xtremecraft/controlador/AccionConstruirFabrica.java
@@ -9,18 +9,19 @@ import xtremecraft.mapa.NoSePudoOcuparElTerrenoException;
 import xtremecraft.mapa.Tierra;
 import xtremecraft.partida.Jugador;
 import xtremecraft.partida.Partida;
+import xtremecraft.raza.RazaNoTieneBarracasException;
 import xtremecraft.raza.RecursosInsuficientesException;
 import xtremecraft.vista.MensajeDeError;
 
 @SuppressWarnings("serial")
-public class AccionConstruirBarraca extends AbstractAction{
+public class AccionConstruirFabrica extends AbstractAction{
 
 	private Partida partida;
 	private Coordenada coordenada;
 	
-	public AccionConstruirBarraca(Partida partida, Coordenada coordenada){
+	public AccionConstruirFabrica(Partida partida, Coordenada coordenada){
 		
-		super("ConstruirBarraca");
+		super("ConstruirFabrica");
 		this.coordenada = coordenada;
 		this.partida = partida;
 		
@@ -31,11 +32,13 @@ public class AccionConstruirBarraca extends AbstractAction{
 		Jugador jugadorActual = partida.quienJuega();
 		Tierra tierraConstruccion = (Tierra) partida.getMapa().getCeldaEnFilaColumna(coordenada.fila(), coordenada.columna()).getCapaInferior();
 		try{
-			jugadorActual.crearBarraca(tierraConstruccion);	
+			jugadorActual.crearFabrica(tierraConstruccion);	
 		}catch(RecursosInsuficientesException noHayRecursos){
 			 new MensajeDeError("No se tienen suficientes recursos");
 		}catch(NoSePudoOcuparElTerrenoException terrenoOcupado){
 			new MensajeDeError("El terreno seleccionado no está disponible para construir");
+		}catch(RazaNoTieneBarracasException noHayBarracas){
+			new MensajeDeError("Se necesita una barraca para crear una fabrica");
 		}
 	}
 	
