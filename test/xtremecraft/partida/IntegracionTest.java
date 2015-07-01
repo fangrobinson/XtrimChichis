75774e66785e4161aa12d25fec713b4607b08866
Robinson Fang <fang.robinson@gmail.com>
2015-07-01 14:10:16 -0300

    Pruebas de Integracion

--- test/xtremecraft/partida/IntegracionTest.java
+++ test/xtremecraft/partida/IntegracionTest.java
@@ -1,23 +1,29 @@
 package xtremecraft.partida;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
 import org.junit.Test;
 
-//import xtremecraft.mapa.Mapa;
-//import xtremecraft.mapa.Terreno;
-//import xtremecraft.mapa.Tierra;
+import xtremecraft.edificios.Barraca;
+import xtremecraft.edificios.Fabrica;
+import xtremecraft.edificios.PuertoEstelar;
+import xtremecraft.edificios.RecolectorDeGasVespeno;
+import xtremecraft.edificios.RecolectorDeMineral;
+import xtremecraft.mapa.Mapa;
+import xtremecraft.mapa.Terreno;
 import xtremecraft.partida.Jugador;
 import xtremecraft.partida.Partida;
-//import xtremecraft.recursos.MinaDeMinerales;
+import xtremecraft.unidades.Marine;
 
 
 public class IntegracionTest {
 	
 	@Test
-	public void prueba01CreacionDeRecolectoresDeGasDeUnidadesAtaqueEntreUnidadesYaEdificios(){
+	public void prueba01CreacionDeEdificiosYAtaquesValidos(){
 		
 		ArrayList<String> nombresJugadores = new ArrayList<String>();
 		nombresJugadores.add("noob");
@@ -31,64 +37,109 @@ public class IntegracionTest {
 		
 		Jugador jugadorTurno = xtremGame.quienJuega();
 		
-		//Mapa mapa = xtremGame.getMapa();
+		int recursoMineral = jugadorTurno.getCantidadDeMinerales();
 		
-		//Terreno esteTerreno = mapa.obtenerTerrenoConMinaDeMinerales();
+		Mapa mapa = xtremGame.getMapa();
 
-		jugadorTurno.nacion().juntarMinerales(200);
+		Terreno unTerreno = mapa.getCeldaEnFilaColumna(2, 4).getCapaInferior(); 
 		
-		//xtremGame.crearRecolectorDeMineral(jugadorTurno,esteTerreno.fila(),esteTerreno.columna());
+		RecolectorDeMineral recolector = jugadorTurno.crearRecolectorDeMineral(unTerreno);
 		
-		xtremGame.pasarTiempo();
+		assertTrue(jugadorTurno.getCantidadDeMinerales() < recursoMineral);
 		
-		jugadorTurno.pasarTurno();
+		//Lo mismo para el otro jugador
+		xtremGame.quienJuega().pasarTurno();
+		unTerreno = mapa.getCeldaEnFilaColumna(2, 6).getCapaInferior(); 
+		RecolectorDeMineral recolector2 = xtremGame.quienJuega().crearRecolectorDeMineral(unTerreno);
+		xtremGame.quienJuega().pasarTurno();
+		//Volvemos al jugador inical
 		
-        nombreJugadorTurno = xtremGame.quienJuega().nombre();
+		assertTrue(recolector.estaEnConstruccion());
+		assertTrue(recolector2.estaEnConstruccion());
 		
-		assertEquals(nombreJugadorTurno, "playerOpCoreano");
-		
-	}
-
-	/*
-	@Test
-	
-	public void prueba03_creacionDeRecolectoresDeGasDeUnidadesAtaqueEntreUnidadesYaEdificios(){
-
-		//assertEquals(nombreJugadorTurno, "playerOpCoreano");
-        
-        for (int i = 0; i < 10; i++){
-        	xtremGame.pasarTiempo(); //Espero a que se construyan ambos recolectores y a que stocken minerales
-        }
-        
-        nombreJugadorTurno = xtremGame.quienJuega().nombre();
-		
+		for(int j = 0; j < 40; j++){
+			xtremGame.quienJuega().pasarTurno();
+		}
+		nombreJugadorTurno = xtremGame.quienJuega().nombre();
 		assertEquals(nombreJugadorTurno, "noob");
+		assertFalse(recolector.estaEnConstruccion());
+		assertFalse(recolector2.estaEnConstruccion());
+		assertTrue(jugadorTurno.getCantidadDeMinerales() > recursoMineral);
 		
-		jugadorTurno = xtremGame.quienJuega();
-		IteradorEdificios iter = new IteradorEdificios(jugadorTurno.nacion().edificios());
-		Barraca unaBarraca = (Barraca) iter.nextDe(Barraca.class);
-		//xtremGame.crearMarine(jugadorTurno, unaBarraca);
+		xtremGame.quienJuega().pasarTurno();
+		assertTrue(xtremGame.quienJuega().getCantidadDeMinerales() > recursoMineral);
 		
-        xtremGame.pasarTiempo();
 		
-        nombreJugadorTurno = xtremGame.quienJuega().nombre();
+		recursoMineral = xtremGame.quienJuega().getCantidadDeMinerales();
+		unTerreno = mapa.getCeldaEnFilaColumna(0, 0).getCapaInferior(); 
+		Barraca barracaJugadorUno = xtremGame.quienJuega().crearBarraca(unTerreno);
 		
-		//assertEquals(nombreJugadorTurno, "playerOpCoreano");
-        
-        for (int i = 0; i < 10; i++){
-        	//xtremGame.crearMarine(jugadorTurno, unaBarraca);
-        }
-        
-        
-        for (int i = 0; i < 2; i++){
-        	xtremGame.pasarTiempo();   //espero que se construyan los marines
-        }
-        
-        
-        
-        for (int i = 0; i < 30; i++){
-        	xtremGame.pasarTiempo();   //espero que se construyan los marines
-        }
+		assertTrue(xtremGame.quienJuega().getCantidadDeMinerales() < recursoMineral);
+
+		xtremGame.quienJuega().pasarTurno();
+		recursoMineral = xtremGame.quienJuega().getCantidadDeMinerales();
+		unTerreno = mapa.getCeldaEnFilaColumna(0, 1).getCapaInferior(); 
+		Barraca barracaJugadorDos = xtremGame.quienJuega().crearBarraca(unTerreno);
+		assertTrue(xtremGame.quienJuega().getCantidadDeMinerales() < recursoMineral);
+		
+		assertTrue(barracaJugadorUno.estaEnConstruccion());
+		assertTrue(barracaJugadorDos.estaEnConstruccion());
+		
+		for(int j = 0; j < 40; j++){
+			xtremGame.quienJuega().pasarTurno();
+		}
+		
+		assertFalse(barracaJugadorUno.estaEnConstruccion());
+		assertFalse(barracaJugadorDos.estaEnConstruccion());
+		
+		Marine marineUno = xtremGame.quienJuega().crearMarine(barracaJugadorDos, mapa);
+		
+		assertTrue(marineUno.estaEnConstruccion());
+		
+		for(int j = 0; j < 40; j++){
+			xtremGame.quienJuega().pasarTurno();
+		}
+		
+		assertFalse(marineUno.estaEnConstruccion());
+		
+		unTerreno = mapa.getCeldaEnFilaColumna(0, 2).getCapaInferior(); 
+		Fabrica fabricaDos = xtremGame.quienJuega().crearFabrica(unTerreno);
+		assertTrue(fabricaDos.estaEnConstruccion());
+		for(int j = 0; j < 40; j++){
+			xtremGame.quienJuega().pasarTurno();
+		}
+		assertFalse(fabricaDos.estaEnConstruccion());
+		
+		unTerreno = mapa.getCeldaEnFilaColumna(1, 10).getCapaInferior();
+		RecolectorDeGasVespeno recolectorDeGasDos = xtremGame.quienJuega().crearRecolectorDeGasVespeno(unTerreno);
+		assertTrue(recolectorDeGasDos.estaEnConstruccion());
+		for(int j = 0; j < 40; j++){
+			xtremGame.quienJuega().pasarTurno();
+		}
+		assertFalse(recolectorDeGasDos.estaEnConstruccion());
+			
+		unTerreno = mapa.getCeldaEnFilaColumna(0, 3).getCapaInferior(); 
+		PuertoEstelar unPuertoDos = xtremGame.quienJuega().crearPuertoEstelar(unTerreno);
+		assertTrue(unPuertoDos.estaEnConstruccion());
+		for(int j = 0; j < 40; j++){
+			xtremGame.quienJuega().pasarTurno();
+		}
+		assertFalse(unPuertoDos.estaEnConstruccion());
+		
+		int vidaMarine = marineUno.getVida();
+		int vidaBarraca = barracaJugadorUno.getVida();
+		xtremGame.quienJuega().atacar(marineUno, barracaJugadorUno);
+		assertTrue(vidaBarraca > barracaJugadorUno.getVida());
+		vidaBarraca = barracaJugadorUno.getVida();
+		
+		xtremGame.quienJuega().pasarTurno();
+		xtremGame.quienJuega().pasarTurno();
+		
+		xtremGame.quienJuega().atacar(marineUno, barracaJugadorUno);
+		assertTrue(vidaBarraca > barracaJugadorUno.getVida());
+		vidaBarraca = barracaJugadorUno.getVida();
+		
+		assertEquals(marineUno.getVida(), vidaMarine);
 	}
-		*/
+
 }
